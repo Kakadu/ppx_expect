3437e7c4a28cc5c3811fe58e83f13b77a1aaf0e3
Kakadu <Kakadu@pm.me>
2025-10-29 16:18:16 +0300

    Remove leftovers about base and ppx_here
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- src/ppx_expect.ml
+++ src/ppx_expect.ml
@@ -194,10 +194,12 @@ let replace_and_collect_expects =
   end
 ;;
 
+let expand_filename fname = fname
+
 let transform_let_expect ~trailing_location ~tags ~expected_exn ~description ~loc body =
   let body, expectations = replace_and_collect_expects#expression body [] in
   let filename_rel_to_project_root =
-    Ppx_here_expander.expand_filename loc.loc_start.pos_fname
+    expand_filename loc.loc_start.pos_fname
   in
   let trailing_location = compact_loc_of_ppxlib_location trailing_location in
   let body_loc =
@@ -314,7 +316,7 @@ let () =
         *)
         let loc = { loc with loc_ghost = true } in
         let filename_rel_to_project_root =
-          Ppx_here_expander.expand_filename loc.loc_start.pos_fname
+           expand_filename loc.loc_start.pos_fname
         in
         let header =
           let loc = { loc with loc_end = loc.loc_start } in
